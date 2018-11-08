5874b50c60ed21b6ab0f2f086eed688f45eecd29
Jul Kreshpaj <jul@paperclicks.net>
2018-11-08 09:49:11 +0100

    add package to file

---
+++ cmd/actions.go
@@ -0,0 +1,84 @@
+package cmd
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+// ? Ask for package number to be installed
+func makePrompt() {
+	installed := make(chan bool)
+	packageNumber := makeQuestion()
+	progress.Prefix = "➡ Installing package " + packages.Hits[packageNumber].Name + " from " + packages.Hits[packageNumber].Package + " "
+
+	progress.Start()
+	go makeInstall(packages, packageNumber, installed)
+	<-installed
+	progress.Stop()
+
+	success.Println("➡ Successfully installed", packages.Hits[packageNumber].Name)
+}
+
+// ? Go get package
+func makeInstall(packages *Packages, packageNumber int, installed chan bool) {
+	cmd := exec.Command("go", "get", packages.Hits[packageNumber].Package)
+	cmd.Run()
+
+	saveToFile(packages, packageNumber)
+	installed <- true
+}
+
+// ? Make package number question
+func makeQuestion() int {
+	fmt.Print("➡ Enter number of package to be installed: ")
+	input := bufio.NewScanner(os.Stdin)
+	input.Scan()
+	packageNumber, _ := strconv.Atoi(input.Text())
+
+	// Check if input is int if not remake question
+	if _, err := strconv.Atoi(input.Text()); err != nil {
+		failed.Println("Please type a package number or Ctrl+C to cancel")
+		return makeQuestion()
+	}
+
+	return packageNumber - 1
+}
+
+// ? Save installed package to file
+func saveToFile(packages *Packages, packageNumber int) {
+	newPackage := make(map[string]interface{})
+
+	// Open packages.json
+	file, err := os.OpenFile("packages.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		failed.Println("Error opening packages.json")
+	}
+	defer file.Close()
+
+	// Read packages.json
+	depsByte, err := ioutil.ReadFile("packages.json")
+	if err != nil {
+		failed.Println("Error reading packages.json")
+	}
+	var result map[string]interface{}
+	json.Unmarshal([]byte(depsByte), &result)
+	if result == nil {
+		newPackage[strings.Split(packages.Hits[packageNumber].Name, " ")[1]] = packages.Hits[packageNumber].Package
+		result = newPackage
+	} else {
+		result[strings.Split(packages.Hits[packageNumber].Name, " ")[1]] = packages.Hits[packageNumber].Package
+	}
+
+	// Write packages.json
+	deps, _ := json.MarshalIndent(result, "", "\t")
+	err = ioutil.WriteFile("packages.json", deps, 0644)
+	if err != nil {
+		failed.Println("Error writing to packages.json")
+	}
+}
--- cmd/packges.go
+++ cmd/packges.go
@@ -1,31 +1,15 @@
 package cmd
 
 import (
-	"bufio"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
-	"os/exec"
 	"strconv"
-	"strings"
-	"time"
-
-	"github.com/briandowns/spinner"
-	"github.com/fatih/color"
 )
 
-var (
-	s        = spinner.New(spinner.CharSets[7], 100*time.Millisecond)
-	p        = spinner.New(spinner.CharSets[7], 100*time.Millisecond)
-	success  = color.New(color.FgGreen)
-	failed   = color.New(color.FgRed)
-	packages = new(Packages)
-)
-
-//Package type contains all the package information
+// ? Package type contains all the package information
 type Package struct {
 	Name        string `json:"name"`
 	Synopsis    string `json:"synopsis"`
@@ -35,26 +19,22 @@ type Package struct {
 	ProjectURL  string `json:"projecturl"`
 }
 
-//Dependence package
-type Dependence struct {
-	Name string
-}
-
-//Packages type contains search query and list of packages
+// ? Packages type contains search query and list of packages
 type Packages struct {
 	Query string    `json:"query"`
 	Hits  []Package `json:"hits"`
 }
 
+// ? Search api for package and populate table
 func searchPackages(q string, done chan bool) {
 	res, err := http.Get("https://go-search.org/api?action=search&q=" + q)
 	if err != nil {
-		log.Println("ERROR MAKING REQUEST")
+		failed.Println("Error making search request")
 	}
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		fmt.Println("ERROR READING BODY")
+		failed.Println("Failed to read response body")
 	}
 	json.Unmarshal([]byte(body), &packages)
 
@@ -68,14 +48,15 @@ func searchPackages(q string, done chan bool) {
 	done <- true
 }
 
-func populateTable(pcks *Packages) {
+// ? Add pakages result to table
+func populateTable(packages *Packages) {
 	w, h := getTSize()
-	boxArr := drawBox(w, h)
-	for i, row := range boxArr {
+	box := drawBox(w, h)
+	for i, row := range box {
 		if i > 1 {
-			if i-2 < len(boxArr)-3 {
-				if len(pcks.Hits) > i-2 {
-					writeToRow(row, pcks.Hits[i-2])
+			if i-2 < len(box)-3 {
+				if len(packages.Hits) > i-2 {
+					writeToRow(row, packages.Hits[i-2])
 				} else {
 					writeToRow(row, Package{Name: "", Synopsis: "", Author: ""})
 				}
@@ -84,67 +65,5 @@ func populateTable(pcks *Packages) {
 			*row = setTitle(row)
 		}
 		fmt.Println(row.String())
-	}
-}
-
-func makePrompt() {
-	installed := make(chan bool)
-	pn := makeQuestion()
-	p.Prefix = "➡ Installing package " + packages.Hits[pn-1].Name + " from " + packages.Hits[pn-1].Package + " "
-	p.Start()
-	go makeInstall(packages, pn, installed)
-	<-installed
-	p.Stop()
-	success.Println("➡ Successfully installed", packages.Hits[pn-1].Name)
-}
-
-func makeInstall(pcks *Packages, pn int, installed chan bool) {
-	cmd := exec.Command("go", "get", pcks.Hits[pn-1].Package)
-	cmd.Run()
-	saveToFile(pcks, pn)
-	installed <- true
-}
-
-func makeQuestion() int {
-	fmt.Print("➡ Enter number of package to be installed: ")
-	input := bufio.NewScanner(os.Stdin)
-	input.Scan()
-	pn, _ := strconv.Atoi(input.Text())
-	if _, err := strconv.Atoi(input.Text()); err != nil {
-		failed.Println("Please type a package number or Ctrl+C to cancel")
-		return makeQuestion()
-	}
-	return pn
-}
-
-func saveToFile(pcks *Packages, pn int) {
-	newPck := make(map[string]interface{})
-
-	//Open packages.json
-	file, err := os.OpenFile("packages.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
-	if err != nil {
-		failed.Println("Error opening packages.json")
-	}
-	defer file.Close()
-
-	//Read packages.json
-	depsByte, err := ioutil.ReadFile("packages.json")
-	if err != nil {
-		failed.Println("Error reading packages.json")
-	}
-	var result map[string]interface{}
-	json.Unmarshal([]byte(depsByte), &result)
-	if result == nil {
-		newPck[strings.Split(pcks.Hits[pn-1].Name, " ")[1]] = pcks.Hits[pn-1].Package
-		result = newPck
-	} else {
-		result[strings.Split(pcks.Hits[pn-1].Name, " ")[1]] = pcks.Hits[pn-1].Package
-	}
-
-	//Write packages.json
-	deps, _ := json.MarshalIndent(result, "", "\t")
-	err = ioutil.WriteFile("packages.json", deps, 0644)
-	if err != nil {
-		failed.Println("Error writing to packages.json")
 	}
 }
